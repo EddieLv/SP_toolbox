ac949b35886bb06dc1c607b8ecfa682d9d6ebf1e
EddieLv <61786787+EddieLv@users.noreply.github.com>
2023-05-16 17:21:34 +0800

    Add files via upload

---
+++ STvis/R/zzz.R
@@ -0,0 +1,12 @@
+.onAttach <- function(libname, pkgname) {
+  cat(" __        __     _                                _            ____  _   _  _____  _   _  _         _     ____
+ \ \      / /___ | |  ___  ___   _ __ ___    ___  | |_  ___    / ___|| | | || ____|| \ | || |       / \   | __ )
+  \ \ /\ / // _ \| | / __|/ _ \ | '_ ` _ \  / _ \ | __|/ _ \  | |    | |_| ||  _|  |  \| || |      / _ \  |  _ \
+   \ V  V /|  __/| || (__| (_) || | | | | ||  __/ | |_| (_) | | |___ |  _  || |___ | |\  || |___  / ___ \ | |_) |
+    \_/\_/  \___||_| \___|\___/ |_| |_| |_| \___|  \__|\___/   \____||_| |_||_____||_| \_||_____|/_/   \_\|____/
+
+This package is used for Spatial-Transcriptome visualization(base on Seurat Object)!
+You have to check the correctness of your Seurat Object carefully and contact with us if encountered with problems.
+@Author: lvjunjie -- GZLAB-CHENLAB\n")
+}
+
