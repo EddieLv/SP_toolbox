a633a046b1f2f6c150d24b6dc7a2cf53ab3fbad3
EddieLv <61786787+EddieLv@users.noreply.github.com>
2023-05-16 18:03:18 +0800

    Delete zzz.R

--- STvis/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  cat(" This package is used for Spatial-Transcriptome visualization(base on Seurat Object)!\
-      You have to check the correctness of your Seurat Object carefully and contact with us if encountered with problems.\n
-      @Author: lvjunjie -- GZLAB-CHENLAB\n")
-  }
-
